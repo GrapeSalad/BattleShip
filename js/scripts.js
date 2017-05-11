80298b7fd30cc4531960f4733579e4ee30adb4e3
David Wilson and Kimlan Nguyen <david+kimlan@>
2017-05-11 16:07:54 -0700

    make pretty, add reset button and text to explain the point of the site and credits.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 $(document).ready(function(){
+
   var shipCoordinates = USSJhin.shipPosition(USSJhin);
   var shipCoordinates1 = USSPudge.shipPosition(USSPudge);
-  // for(i=0; i<3; i++){
-  //   console.log(shipCoordinates[i]);
-  // }
-  console.log(shipCoordinates);
+
   $("td.operator").click(function(){
     var bombLocation = parseInt($(this)[0].id);
-    console.log(bombLocation);
-
+//FINDING SHIP ONE
     if(bombLocation === shipCoordinates[0] || bombLocation === shipCoordinates[1] || bombLocation === shipCoordinates[2]){
       if(bombLocation === shipCoordinates[0]){
         $(this).addClass("hitFront");
@@ -20,7 +17,9 @@ $(document).ready(function(){
     }
     else{
       $(this).addClass("hitFail");
-    }if(bombLocation === shipCoordinates1[0] || bombLocation === shipCoordinates1[1] || bombLocation === shipCoordinates1[2]){
+    }
+//FINDING SHIP TWO
+    if(bombLocation === shipCoordinates1[0] || bombLocation === shipCoordinates1[1] || bombLocation === shipCoordinates1[2]){
       if(bombLocation === shipCoordinates1[0]){
         $(this).addClass("hitFront");
       }else if(bombLocation === shipCoordinates1[1]){
@@ -32,6 +31,10 @@ $(document).ready(function(){
     else{
       $(this).addClass("hitFail");
     }
+  });
+
+  $("button#resetButton").click(function() {
+    location.reload();
   });
 });
 
@@ -50,13 +53,9 @@ shipEngine.prototype.randomNumberGenerator = function(min, max){
   return Math.floor(Math.random()*(max-min))+min;
 }
 
-
-//USSJhin(USSJhin.randomNumberGenerator(1,82),USSJhin.randomNumberGenerator(0,2), USSJhin.randomNumberGenerator(0,2));
-
 shipEngine.prototype.shipPosition = function(ship){
   //the ship is 3 squares in length
   var centerCoordinate = ship.randomNumberGenerator(1, 82);
-  console.log(centerCoordinate);
   function isBorderColumn(centerCoordinate){
     var bool = false;
     for(i=0; i<ship.VerticalException.length; i++){
@@ -71,7 +70,6 @@ shipEngine.prototype.shipPosition = function(ship){
     }
     return bool;
   }
-  console.log(isBorderColumn(centerCoordinate));
   //Not including corners
   if(isBorderColumn(centerCoordinate) === false){
     if(centerCoordinate === 1 || centerCoordinate === 9 || centerCoordinate === 73 || centerCoordinate === 81){
@@ -80,27 +78,22 @@ shipEngine.prototype.shipPosition = function(ship){
     //if ship is on border rows, it can only arrange horizontally
     else if(((2<= centerCoordinate) && (centerCoordinate <= 8)) || ((74<= centerCoordinate) && (centerCoordinate <= 80))){
       ship.Coordinates = [centerCoordinate-1, centerCoordinate, centerCoordinate + 1];
-      console.log(ship.Coordinates);
     }
     // If the center coordinate of the ship is on the inner square, it can arrange either horizontally or vertically.
     else{
       var direction = ship.randomNumberGenerator(0,2);
-      console.log(direction);
       // direction 0 = horizontal, direction 1 = vertical
       if(direction === 0){
-        console.log("horizontal");
 
         ship.Coordinates = [centerCoordinate-1, centerCoordinate, centerCoordinate + 1];
       }
       else{
-        console.log("vertical");
         ship.Coordinates = [centerCoordinate-9, centerCoordinate, centerCoordinate + 9];
       }
     }
   }
   else if(isBorderColumn(centerCoordinate) === true){
     ship.Coordinates = [centerCoordinate-9, centerCoordinate, centerCoordinate + 9];
-    console.log("Is an exception: " + ship.Coordinates);
   }
   return ship.Coordinates;
 }
